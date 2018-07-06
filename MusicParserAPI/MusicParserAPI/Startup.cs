62f127eda099efa5565605ec2c062778ac80e889
Dekklin <collinhintzke@outlook.com>
2018-07-05 17:31:02 -0700

    Update Startup.cs

--- MusicParserAPI/MusicParserAPI/Startup.cs
+++ MusicParserAPI/MusicParserAPI/Startup.cs
@@ -32,7 +32,7 @@ namespace MusicParserAPI
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
             });
             services.AddDbContext<MusicDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
